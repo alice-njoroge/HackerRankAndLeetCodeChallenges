4e603ae0fe3ec839ed42278e0de2eea4c8663aba
Alice Wanjiku <alicenjoroge707@gmail.com>
2024-09-03 06:34:46 +0300

    solution alternative if you don't know toString takes an optional radix

--- codeKatas/solution.js
+++ codeKatas/solution.js
@@ -1,12 +1,25 @@
 function rgb(r, g, b){
-    function toHex (value){
-        if (value > 255) value = 255;
-        if (value < 0) value = 0;
+    function hex(value){
+        if(value < 0) value = 0;
+        if(value > 255) value = 255;
 
-        let hex = value.toString(16).toUpperCase();
-        return hex.length === 1 ? '0'+ hex : hex;
+        let quotient = Math.trunc(value / 16);
+        let remainder = value % 16
+
+        return toHex(quotient)+toHex(remainder);
+    }
+
+    function toHex(val){
+        if (val === 10) return 'A';
+        else if (val === 11) return 'B';
+        else if (val === 12) return 'C';
+        else if (val === 13) return 'D';
+        else if (val === 14) return 'E';
+        else if (val === 15) return 'F';
+        else return val;
 
     }
-    return toHex(r)+toHex(g)+toHex(b);
+
+    return hex(r)+hex(g)+hex(b);
 }
 console.log(rgb(255,300, 10));
