d976355bd80eec84b00aeac5f9e5f65bebb29084
Alice Wanjiku <alicenjoroge707@gmail.com>
2024-09-06 14:29:32 +0300

    find killer with only loops - no predefined js methods

--- codeKatas/findKiller/solution.js
+++ codeKatas/findKiller/solution.js
@@ -1,8 +1,32 @@
 function killer(suspectInfo, dead) {
-    for (const key in suspectInfo) {
-        const allFound = dead.every(item => suspectInfo[key].includes(item));
-        if (allFound) return key;
+  //attempt without Js predefined functions
+    //get the individual array items from each object key for key in object assign them to a local variable
+    //loop through the dead array items and find if each item in dead is found in suspects arrays (another loop)
+
+    for(const objKey in suspectInfo){
+        const suspectData = suspectInfo[objKey];
+        let allFound = true; //true to assume that the current suspect might be the killer the next logic will try to disapprove it
+
+        for(let i=0; i < dead.length; i++){ // looping through the victims list
+            const currVictim = dead[i];
+            let found = false;
+
+            for(let j =0; j < suspectData.length; j++){ //looping through the array with suspect data that a single suspect has seen
+                if (currVictim === suspectData[j]){
+                    found = true;
+                    break;// exit the inner loop since the dead person has been found
+                }
+            }
+            if (!found){
+                allFound = false; //current suspect is not the killer
+                break; //Exit the loop since the suspect is disqualified
+            }
+        }
+        if (allFound){
+            return objKey;
+        }
     }
+    return "these are innocent!"
 }
 
 const info = {
