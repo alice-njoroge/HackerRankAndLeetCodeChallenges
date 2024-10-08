c09e05b7fc35c015330a2ec855f61c96ab16ed2e
Alice Wanjiku <alicenjoroge707@gmail.com>
2024-10-08 06:35:04 +0300

    combining objects

--- codeKatas/combineObjects/solution.js
+++ codeKatas/combineObjects/solution.js
@@ -1,8 +1,16 @@
-function digitalRoot(n) {
-    while (n >= 10 ){
-        n = n.toString().split('').reduce((acc, curr) => acc + parseInt(curr), 0);
+function combine(...objects) { //rest operator is going to put all the params into an array
+    const combinedObj = {};
+    for (const iteratee of objects) {
+        for (let currKey in iteratee) {
+            if (combinedObj[currKey]) { //check if the current key already exists and sum the numbers
+                combinedObj[currKey] += iteratee[currKey]
+            } else {
+                combinedObj[currKey] = iteratee[currKey];
+            }
+        }
     }
-    return n;
-}
 
-console.log(digitalRoot(1990));
+    return combinedObj;
+
+}
+console.log(combine({ a: 10, b: 20, c: 30 }, { a: 3, c: 6, d: 3 }));
